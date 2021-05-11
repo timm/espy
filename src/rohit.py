096dce14d41a50fbbc8fc4948adb449729e35ede
Xiao Ling <41660376+XiaoLing941212@users.noreply.github.com>
2021-05-11 17:15:35 -0400

    Add files via upload

--- src/rohit.py
+++ src/rohit.py
@@ -20,45 +20,65 @@ def read_config(file):
 ### - input:                       ###
 ###    - config: configuration     ###
 ###    - n: number of sims to gen  ###
+###    - test: whether run tests   ###
 ### - output:                      ###
 ###    - sims: a list of sims      ###
-def gen_sim(config, n):
+def gen_sim(config, n, test):
     sims = []
     
-    # iterate n sims
-    for i in range(n):
-        temp_sim = []
-        
-        # iterate through dependent variables and generate random number for each of them
-        for key, item in enumerate(config):
-            temp_id = "v" + str(key+1)
-            temp_name = item[temp_id]['name']
-            temp_min_value = item[temp_id]['min_value']
-            temp_max_value = item[temp_id]['max_value']
+    if test == False:
+        progress_count = 0
+        # iterate n sims
+        while progress_count < n:
+            temp_sim = []
             
-            # descend angle 2 is always 3 less than descend angle 1, handle this
-            if temp_name == "descend_angle_1":
-                temp_angle_1 = random.randint(temp_min_value, temp_max_value)
-                temp_angle_2 = temp_angle_1 - 3
-                temp_sim.append(temp_angle_1)
-                temp_sim.append(temp_angle_2)
-            elif temp_name == "descend_angle_2":
-                continue
-            else:
-                temp_sim.append(random.randint(temp_min_value, temp_max_value))
+            # iterate through dependent variables and generate random number for each of them
+            for key, item in enumerate(config):
+                temp_id = "v" + str(key+1)
+                temp_name = item[temp_id]['name']
+                temp_min_value = item[temp_id]['min_value']
+                temp_max_value = item[temp_id]['max_value']
+                
+                # descend angle 2 is always 3 less than descend angle 1, handle this
+                if temp_name == "descend_angle_1":
+                    temp_angle_1 = random.randint(temp_min_value, temp_max_value)
+                    temp_angle_2 = temp_angle_1 - 3
+                    temp_sim.append(temp_angle_1)
+                    temp_sim.append(temp_angle_2)
+                elif temp_name == "descend_angle_2":
+                    continue
+                else:
+                    temp_sim.append(random.randint(temp_min_value, temp_max_value))
+            
+            sims.append(goal_cal(temp_sim))
+            progress_count += 1
+    else:
+        test_sims = [[7,10,7,41,6323,607,1166,11,17], [3,28,25,60,26451,720,1009,16,132], [10,30,27,47,27011,599,732,19,20], [26,23,20,72,23183,574,704,10,176], [17,16,13,46,15801,931,548,0,4], 
+                    [23,18,15,82,10567,200,465,29,88], [24,23,20,25,24912,504,366,15,158], [3,30,27,41,16294,935,196,29,176], [29,19,16,28,22699,436,1239,23,176], [21,21,18,102,6571,312,1380,29,23]]
         
-        sims.append(temp_sim)
+        for item in test_sims:
+            sims.append(goal_cal(item))
+        
+        print("")
+        print("-----TESTING MODE-----")
+
+        print("right outputs:")
+        result_test_sims = [[7,10,7,41,6323,607,1166,11,17,1,0,0,0,0], [3,28,25,60,26451,720,1009,16,132,0,0,0,0,0], [10,30,27,47,27011,599,732,19,20,0,2,1,1,0], [26,23,20,72,23183,574,704,10,176,0,1,2,0,0], 
+                            [17,16,13,46,15801,931,548,0,4,0,0,0,0,0], [23,18,15,82,10567,200,465,29,88,0,3,3,3,0], [24,23,20,25,24912,504,366,15,158,1,0,0,0,1], [3,30,27,41,16294,935,196,29,176,1,1,0,0,0],
+                            [29,19,16,28,22699,436,1239,23,176,1,0,0,0,1], [21,21,18,102,6571,312,1380,29,23,0,3,3,4,0]]
+        header_row = ['Ascend_angle', 'Descend_angle_1', 'Descend_angle_2', 'Cruise_speed', 'Trip_distance', 'Cruise_altitude', 'Payload', 'Wind', 'Direction', 'T-', 'Long_accel-', 'Lat_accel-', 'Jerk-', 'Charging-']
+        [print(', '.join(x for x in header_row))]
+        [print(', '.join([str(x) for x in lst])) for lst in result_test_sims]
 
     return sims
 
 
 ### fun: calculate t, longitudnal_accel, lateral_accel, jerk, and energy ###
 ### - input:                                                             ###
-###    - sims: a list of sims generated previous                         ###
+###    - sim: a simulation variables                                     ###
 ### - output:                                                            ###
-###    - sims_goal: a list of sims with goals variables calculated       ###
-def goal_cal(sims): 
-    sim_goal = []
+###    - sim_goal: the goals of the input sim                            ###
+def goal_cal(sim): 
     MTOW = 5000
     hover = 58
     cruise = 12.5
@@ -67,92 +87,91 @@ def goal_cal(sims):
     p5z = 150
     p6z = 5
 
-    for item in sims:
-        # phase 1
-        p1t = 3
-        p1x, p1y, p1z = 0, 0, 5
-        v1x, v1y, v1z = 0, 0, p1z/p1t
-        p1long = p1z*2/(p1t**2)
-        p1lat = p1x/p1t
-        p1jerk = math.sqrt((p1lat/p1t) ** 2 + (p1long/p1t) ** 2)
-        p1energy = ((MTOW+item[6])/1000)**1.5 * hover * p1t / 3600
+    # phase 1
+    p1t = 3
+    p1x, p1y, p1z = 0, 0, 5
+    v1x, v1y, v1z = 0, 0, p1z/p1t
+    p1long = p1z*2/(p1t**2)
+    p1lat = p1x/p1t
+    p1jerk = math.sqrt((p1lat/p1t) ** 2 + (p1long/p1t) ** 2)
+    p1energy = ((MTOW+sim[6])/1000)**1.5 * hover * p1t / 3600
         
-        # phase 2
-        p2t = 5
-        p2x, p2y, p2z = 35, 0, 5
-        v2x, v2y, v2z = p2x/p2t + item[7] * math.cos(math.radians(item[8])), 0, 0
-        p2long = 0
-        p2lat = (p2x - p1x) * 2 / (p2t**2)
-        p2jerk = math.sqrt(((p2lat-p1lat)/p2t)**2 + ((p2long-p1long)/p2t)**2)
-        p2energy = (MTOW+item[6]) * cruise / 1000 * p2t / 3600 * (item[3]/67)
+    # phase 2
+    p2t = 5
+    p2x, p2y, p2z = 35, 0, 5
+    v2x, v2y, v2z = p2x/p2t + sim[7] * math.cos(math.radians(sim[8])), 0, 0
+    p2long = 0
+    p2lat = (p2x - p1x) * 2 / (p2t**2)
+    p2jerk = math.sqrt(((p2lat-p1lat)/p2t)**2 + ((p2long-p1long)/p2t)**2)
+    p2energy = (MTOW+sim[6]) * cruise / 1000 * p2t / 3600 * (sim[3]/67)
         
-        # phase 3
-        p3z = item[5]
-        p3y = 0
-        p3x = p2x + (p3z - p2z) / math.tan(math.radians(item[0]))
-        v3x = item[3] + item[7] * math.cos(math.radians(item[8]))
-        v3y = 0
-        p3t = (p3x - p2x) * 2 / (v3x + v2x)
-        v3z = (p3z - p2z) / p3t
-        p3long = (p3z - p2z) * 2 / (p3t**2)
-        p3lat = ((v3x**2) - (v2x**2)) / (2 * (p3x - p2x))
-        p3jerk = math.sqrt(((p3lat-p2lat)/p3t)**2 + ((p3long-p2long)/p3t)**2)
-        p3energy = (MTOW+item[6]) * cruise / 1000 * p3t / 3600 * (item[3]/67)
+    # phase 3
+    p3z = sim[5]
+    p3y = 0
+    p3x = p2x + (p3z - p2z) / math.tan(math.radians(sim[0]))
+    v3x = sim[3] + sim[7] * math.cos(math.radians(sim[8]))
+    v3y = 0
+    p3t = (p3x - p2x) * 2 / (v3x + v2x)
+    v3z = (p3z - p2z) / p3t
+    p3long = (p3z - p2z) * 2 / (p3t**2)
+    p3lat = ((v3x**2) - (v2x**2)) / (2 * (p3x - p2x))
+    p3jerk = math.sqrt(((p3lat-p2lat)/p3t)**2 + ((p3long-p2long)/p3t)**2)
+    p3energy = (MTOW+sim[6]) * cruise / 1000 * p3t / 3600 * (sim[3]/67)
         
-        # phase 4
-        p4z = item[5]
-        p4y = 0
-        p4x = item[4] - (p4z - p5z) / math.tan(math.radians(item[2])) - (p5z - p6z) / math.tan(math.radians(item[1]))
-        v4x = item[3] + item[7] * math.cos(math.radians(item[8]))
-        v4y = 0
-        v4z = 0
-        p4t = (p4x - p3x) * 2 / (v4x + v3x)
-        p4long = (p4z - p3z) * 2 / (p4t**2)
-        p4lat = ((v4x**2) - (v3x**2)) / (2 * (p4x - p3x))
-        p4jerk = math.sqrt(((p4lat-p3lat)/p4t)**2 + ((p4long-p3long)/p4t)**2)
-        p4energy = (MTOW+item[6]) * cruise / 1000 * p4t / 3600 * (item[3]/67)
+    # phase 4
+    p4z = sim[5]
+    p4y = 0
+    p4x = sim[4] - (p4z - p5z) / math.tan(math.radians(sim[2])) - (p5z - p6z) / math.tan(math.radians(sim[1]))
+    v4x = sim[3] + sim[7] * math.cos(math.radians(sim[8]))
+    v4y = 0
+    v4z = 0
+    p4t = (p4x - p3x) * 2 / (v4x + v3x)
+    p4long = (p4z - p3z) * 2 / (p4t**2)
+    p4lat = ((v4x**2) - (v3x**2)) / (2 * (p4x - p3x))
+    p4jerk = math.sqrt(((p4lat-p3lat)/p4t)**2 + ((p4long-p3long)/p4t)**2)
+    p4energy = (MTOW+sim[6]) * cruise / 1000 * p4t / 3600 * (sim[3]/67)
             
-        # phase 5
-        p5z = 150
-        p5y = 0
-        p5x = p4x + (p4z - p5z) / math.tan(math.radians(item[2]))
-        v5x = v4x/2 + item[7] * math.cos(math.radians(item[8]))
-        v5y = 0
-        v5z = v5x * (p4z - p5z) / (p5x - p4x)
-        p5t = (p5x - p4x) * 2 / (v5x + v4x)
-        p5long = (p5z - p4z) * 2 / (p5t**2)
-        p5lat = ((v5x**2) - (v4x**2)) / (2 * (p5x - p4x))
-        p5jerk = math.sqrt(((p5lat-p4lat)/p5t)**2 + ((p5long-p4long)/p5t)**2)
-        p5energy = (MTOW+item[6]) * cruise / 1000 * p5t / 3600 * (item[3]/67)
+    # phase 5
+    p5z = 150
+    p5y = 0
+    p5x = p4x + (p4z - p5z) / math.tan(math.radians(sim[2]))
+    v5x = v4x/2 + sim[7] * math.cos(math.radians(sim[8]))
+    v5y = 0
+    v5z = v5x * (p4z - p5z) / (p5x - p4x)
+    p5t = (p5x - p4x) * 2 / (v5x + v4x)
+    p5long = (p5z - p4z) * 2 / (p5t**2)
+    p5lat = ((v5x**2) - (v4x**2)) / (2 * (p5x - p4x))
+    p5jerk = math.sqrt(((p5lat-p4lat)/p5t)**2 + ((p5long-p4long)/p5t)**2)
+    p5energy = (MTOW+sim[6]) * cruise / 1000 * p5t / 3600 * (sim[3]/67)
         
-        # phase 6
-        p6z = 5
-        p6y = 0
-        p6x = p5x + (p5z - p6z) / math.tan(math.radians(item[1]))
-        v6x = (v5x + 0) / 2
-        v6y = 0
-        p6t = (p6x - p5x) * 2 / (v6x + v5x)
-        v6z = (p5z - p6z) / p6t
-        p6long = (p6z - p5z) * 2 / (p6t**2)
-        p6lat = ((v6x**2) - (v5x**2)) / (2 * (p6x - p5x))
-        p6jerk = math.sqrt(((p6lat-p5lat)/p6t)**2 + ((p6long-p5long)/p6t)**2)
-        p6energy = (MTOW+item[6]) * cruise / 1000 * p6t / 3600 * (item[3]/67)
+    # phase 6
+    p6z = 5
+    p6y = 0
+    p6x = p5x + (p5z - p6z) / math.tan(math.radians(sim[1]))
+    v6x = (v5x + 0) / 2
+    v6y = 0
+    p6t = (p6x - p5x) * 2 / (v6x + v5x)
+    v6z = (p5z - p6z) / p6t
+    p6long = (p6z - p5z) * 2 / (p6t**2)
+    p6lat = ((v6x**2) - (v5x**2)) / (2 * (p6x - p5x))
+    p6jerk = math.sqrt(((p6lat-p5lat)/p6t)**2 + ((p6long-p5long)/p6t)**2)
+    p6energy = (MTOW+sim[6]) * cruise / 1000 * p6t / 3600 * (sim[3]/67)
         
-        # phase 7
-        p7z = 0
-        p7y = 0
-        p7x = p6x
-        v7x = 0
-        v7y = 0
-        p7t = 3
-        v7z = (p6z - p7z) / p7t
-        p7long = (p7z - p6z) * 2 / (p7t**2)
-        p7lat = (p7x - p6x) / p7t
-        p7jerk = math.sqrt(((p7lat-p6lat)/p7t)**2 + ((p7long-p6long)/p7t)**2)
-        p7energy = ((MTOW+item[6]) / 1000)**1.5 * hover * p7t / 3600
+    # phase 7
+    p7z = 0
+    p7y = 0
+    p7x = p6x
+    v7x = 0
+    v7y = 0
+    p7t = 3
+    v7z = (p6z - p7z) / p7t
+    p7long = (p7z - p6z) * 2 / (p7t**2)
+    p7lat = (p7x - p6x) / p7t
+    p7jerk = math.sqrt(((p7lat-p6lat)/p7t)**2 + ((p7long-p6long)/p7t)**2)
+    p7energy = ((MTOW+sim[6]) / 1000)**1.5 * hover * p7t / 3600
         
-        sim_goal.append([item, [p1t, p2t, p3t, p4t, p5t, p6t, p7t], [p1long, p2long, p3long, p4long, p5long, p6long, p7long], [p1lat, p2lat, p3lat, p4lat, p5lat, p6lat, p7lat], 
-                        [p1jerk, p2jerk, p3jerk, p4jerk, p5jerk, p6jerk, p7jerk], [(p1energy+p2energy+p3energy+p4energy+p5energy+p6energy+p7energy)/((0.2*MTOW)*200/2200)]])
+    sim_goal = [sim, [p1t, p2t, p3t, p4t, p5t, p6t, p7t], [p1long, p2long, p3long, p4long, p5long, p6long, p7long], [p1lat, p2lat, p3lat, p4lat, p5lat, p6lat, p7lat], 
+                [p1jerk, p2jerk, p3jerk, p4jerk, p5jerk, p6jerk, p7jerk], [(p1energy+p2energy+p3energy+p4energy+p5energy+p6energy+p7energy)/((0.2*MTOW)*200/2200)]]
         
     return sim_goal
 
@@ -260,7 +279,7 @@ def goal_generate(sims_violation, choice):
     return sims_final
 
 
-def main(option = None):
+def main(option = None, test = None):
     # define vehicle type (taxi, package, scout)
     vehicle_type = "taxi"
     
@@ -271,17 +290,10 @@ def main(option = None):
     repeat = config["control"]["repeat"]
     
     # generate n sims
-    sims = gen_sim(variable, repeat)       # put 
-    # sims = [[7,10,7,41,6323,607,1166,11,17], [3,28,25,60,26451,720,1009,16,132], [10,30,27,47,27011,599,732,19,20], [26,23,20,72,23183,574,704,10,176], [17,16,13,46,15801,931,548,0,4], [23,18,15,82,10567,200,465,29,88],
-    #         [24,23,20,25,24912,504,366,15,158]]
-
-    # calculate the variables of each goal
-    sims_goal = goal_cal(sims)
-#     print(sims_goal)
+    sims = gen_sim(variable, repeat, test)    
     
     # generate violation
-    sims_vio = violation(sims_goal, threshold, vehicle_type)
-    # print(sims_vio)
+    sims_vio = violation(sims, threshold, vehicle_type)
     
     # generate violation learner table
     ##############################################################################
@@ -292,19 +304,33 @@ def main(option = None):
     ##############################################################################
     choice = 1 
     sims_final = goal_generate(sims_vio, choice)
-    # print(sims_final)
+
+    # pretty print
     header_row = ['Ascend_angle', 'Descend_angle_1', 'Descend_angle_2', 'Cruise_speed', 'Trip_distance', 'Cruise_altitude', 'Payload', 'Wind', 'Direction', 'T-', 'Long_accel-', 'Lat_accel-', 'Jerk-', 'Charging-']
-    
+    print("")
     [print(', '.join(x for x in header_row))]
     [print(', '.join([str(x) for x in lst])) for lst in sims_final]
 
-    # [print(str(lst)) for lst in sims_final]
-
 
 if __name__ == "__main__":
+    print("usage:")
+    print("-c [config file name]: run simulations from specific configuration file")
+    print("-t: run tests")
+
     option = "config.yaml"
+    test = False
 
-    if len(sys.argv) > 1 and sys.argv[1] == "-c":
+    if len(sys.argv) == 4 and sys.argv[1] == "-c" and sys.argv[3] == "-t":
         option = sys.argv[2]
-
-    main(option)
+        test = True
+        main(option, test)
+    elif len(sys.argv) > 1 and sys.argv[1] == "-c":
+        if len(sys.argv) == 2:
+            print("")
+            print("ERROR: please specify the configuration file!")
+        else:
+            option = sys.argv[2]
+            main(option, test)
+    elif len(sys.argv) > 1 and sys.argv[1] == "-t":
+        test = True
+        main(option, test)
