2ee6d355baf45bfcb639c89b8f17282d1a5577df
Thao N <thao.tnguyen@nasa.gov>
2021-07-14 14:09:29 -0700

    added test files

--- src/api.py
+++ src/api.py
@@ -1,9 +1,9 @@
 import copy
 from flask import Flask, request, jsonify
 
-
 from rohit import main
 from flask_cors import CORS
+
 app = Flask(__name__)
 CORS(app)
 
@@ -13,8 +13,69 @@ def bestRules():
     data = request.json
     # print(data['options']["hall"][1]["p2"]["value"])
     # print(data['options'],data['output'] )
-    bestRules = main(data['options'], False, data['output'], json=True)
-    return jsonify(bestRules)
+    # bestRules = main(data['options'], False, data['output'], json=True)
+    bestRules = {}
+    outputs = load_example_files()
+    return jsonify(outputs)
+
+
+import json
+
+
+def load_example_files():
+    basePath = "/Users/ttnguy35/Desktop/espy/src/api/testfiles/sampleData/"
+
+    monitor_joint = json.load(open(basePath + 'monitor_joint.json'))
+    monitor_single = json.load(open(basePath + 'monitor_single.json'))
+    optimize_joint = process_optimize_join(json.load(open(basePath + 'optimize_joint.json')))
+    optimize_single = json.load(open(basePath + 'optimize_single.json'))
+    safety_joint = json.load(open(basePath + 'safety_joint.json'))
+    safety_single = json.load(open(basePath + 'safety_single.json'))
+    bestRules = json.load(open(basePath + 'output-fromZhao.json'))
+
+    return {"monitor_joint": monitor_joint, "monitor_single": monitor_single, "optimize_joint": optimize_joint,
+            "optimize_single": optimize_single, "safety_joint": safety_joint, "safety_single": safety_single, "bestRules": bestRules}
+
+
+def process_optimize_join(monitor_joint):
+    data = monitor_joint
+
+    newData = {}
+
+    # Re-arrange numbers for each entry
+    for key in data.keys():
+        newDataSub = {}
+        # print(data[key])
+
+        # Full dictionary for the data entry pairs
+        nodeDict = {}
+        for key2 in data[key].keys():
+            # Turn X and Values into a x & y pair
+            values = key2.split(", ")
+            node = {}
+            node['x'] = int(values[0])
+            node['y'] = data[key][key2]
+
+            # Sort the nodes by Y rows
+            if values[1] not in nodeDict:
+                nodeDict[values[1]] = []
+            nodeDict[values[1]].append(node)
+            # print(values[1])
+            # print(node)
+
+        # print("nodeDict")
+        # print(nodeDict)
+
+        # Apply to Pair
+        # if key not in newData: newData[key] = []
+        attributes = key.split(", ")
+        newDataSub[attributes[1]] = nodeDict
+
+        if attributes[0] not in newData:
+            newData[attributes[0]] = []
+        newData[attributes[0]].append(newDataSub)
+
+    return newData
 
 
 # def main(option=None, test=None, vehicle=None):
